2ddc33686697f6fd757b1b70b8dafc0319ad3026
s247027 <caovanhieu34@gmail.com>
2025-10-08 10:43:36 +0900

    update 10.0.1043

--- src/in/Main.java
+++ src/in/Main.java
@@ -1,10 +1,36 @@
 package in;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList <student> std = new ArrayList<>() ;
-        new get() ;
+        Scanner sc = new Scanner(System.in);
+        studentManager action = new studentManager();
+        ArrayList<student> std = new ArrayList<>();
+        get ip = new get();
+        boolean run = true;
+
+        do {
+            System.out.println("""
+                    1.Nhap thong tin hoc sinh 
+                    2.In thong tin hien co 
+                    3.Thoat chuong trinh
+                    """);
+            switch (sc.nextInt()) {
+                case 1:
+                    action.addStudent(std, ip.inputName(sc), ip.inputAge(sc), ip.inputAvg(sc));
+                    break;
+                case 2:
+                    action.displayStuden(std);
+                    break;
+                case 3:
+                    run = false;
+                default:
+            }
+
+        } while (run);
+
+
     }
 }
--- src/in/get.java
+++ src/in/get.java
@@ -3,25 +3,72 @@ package in;
 import java.util.Scanner;
 
 public class get {
-    public static String inputName (Scanner sc) {
-        String name ="";
+    public  String inputName(Scanner sc) {
+        String name = "";
         String regexName = "^(?=.*[a-zA-ZÀ-ỹà-ỹ])[a-zA-ZÀ-ỹà-ỹ\\s]+$";
-        boolean run= true;
+        boolean run = true;
 
         do {
             System.out.println("Hay nhap ten ");
             name = sc.nextLine();
             if (!name.matches(regexName)) {
-                System.out.println("Dinh dang ten sai  ");
-            }else {
-                run= false;
+                System.err.println("Dinh dang ten sai  ");
+            } else {
+                run = false;
             }
-        }while (run);
-        return name ;
+        } while (run);
+        return name;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        inputName(sc);
+    public  int inputAge(Scanner sc) {
+        int age = 0;
+        boolean run = true;
+
+        do {
+            try {
+                System.out.println("Hat nhap tuoi ");
+                age = Integer.parseInt(sc.nextLine());
+                if (age > 0) {
+                    run = false;
+                } else {
+                    System.err.println("Tuoi khong duoc am");
+                }
+            } catch (NumberFormatException e) {
+                System.err.println("Hay nhap so nguyen hop le ");
+            }
+
+        } while (run);
+
+
+        return age;
     }
+
+    public  Double inputAvg(Scanner sc) {
+        Double avg = 0.0;
+        boolean run = true;
+
+        do {
+            try {
+                System.out.println("Hay nhap diem trung binh ");
+                avg = Double.parseDouble(sc.nextLine());
+                if (avg < 0 || avg > 10) {
+                    System.err.println("""
+                            Nhap diem khong hop le 
+                            0-10
+                            """);
+                } else {
+                    run = false;
+                }
+            } catch (NumberFormatException e) {
+                System.err.println("Hay nhap so nguyen hop le ");
+            }
+
+        } while (run);
+
+
+        return avg;
+    }
+
+
+
 }
