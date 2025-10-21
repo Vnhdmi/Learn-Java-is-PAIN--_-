0b5dabf405e03078fda915b0b9f6c561bdbc65d6
s247027 <caovanhieu34@gmail.com>
2025-10-21 09:48:12 +0900

    hmm

--- src/H_time/Main.java
+++ src/H_time/Main.java
@@ -1,4 +1,12 @@
 package H_time;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
+    public static void main(String[] args) {
+        LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE dd/MM/yyyy  HH:mm a");
+        System.out.println(time.format(formatter));
+    }
 }
